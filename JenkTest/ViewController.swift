d08b683a28db62877678170d643882030f5d40a3
Neo Din <dinneo@gmail.com>
2017-01-18 23:34:43 +0800

    cool

--- JenkTest/ViewController.swift
+++ JenkTest/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let a = 1
+        _ = a
+        
     }
 
     override func didReceiveMemoryWarning() {
